96edd3e31f0f2d59b4827bda3e318194992e8ba4
anton <kamaeff2@gmail.com>
2023-11-28 00:16:32 +0300

    fix index

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const TelegramApi = require("node-telegram-bot-api");
-//const config = require('./app/Config/config')
 const { config } = require("dotenv");
 const { testDatabaseConnection } = require("./src/app/DB/db");
 
@@ -7,8 +6,6 @@ const startMenu = require("./src/app/startMenu");
 
 config();
 const bot = new TelegramApi(process.env.TOKEN, { polling: true });
-
-//todo: Сделать проверку email (пока не заполнен email пользователь не сможет закакзать пару)
 
 const main = async () => {
   testDatabaseConnection();
@@ -19,10 +16,9 @@ const main = async () => {
     { command: "/start", description: "Перезапуск бота" },
     { command: "/commands", description: "Дополнительные команды бота" },
     { command: "/donate", description: "Поддержать разработчиков" },
-    { command: "/locale", description: "Отправить геолокацию" },
   ]);
 
-  startMenu(bot); // Start menu --> Hello, My profile module, ShowRoom Model and Choose brand Model
+  startMenu(bot);
 };
 
 main();
