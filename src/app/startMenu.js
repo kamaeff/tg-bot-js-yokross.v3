17171c36fa9fe84e7e479059e365ef2eada3cde6
kamaeff <kamaeff2@gmail.com>
2023-12-11 15:08:39 +0300

    fix

--- src/app/startMenu.js
+++ src/app/startMenu.js
@@ -989,7 +989,7 @@ module.exports = (bot) => {
               await bot.deleteMessage(chatId, messageId - 1);
               await bot.sendPhoto(chatId, selectedPhoto[0].path, {
                 caption:
-                  `👟 <b>Кроссовки <i>${selectedPhoto[0].name}</i>\n` +
+                  `👟 <b>Кроссовки <i>${selectedPhoto[0].name}</i></b>\n\n` +
                   `🧵 <b>Характеристики:</b>\n\n` +
                   `➖ <b>Пол:</b> <i>${selectedPhoto[0].gender}</i>\n` +
                   `➖ <b>Цвет:</b> <i>${selectedPhoto[0].color}</i>\n` +
